4737169dce69dbac83ff36d06b01965afca96d21
RoWW4 <rociobeato91@gmail.com>
2020-11-27 15:34:06 +0100

    Añadido comentarios

--- Practica2/Program.cs
+++ Practica2/Program.cs
@@ -11,18 +11,21 @@ public class Practica2
     {
         string adivinar;
         char letra;
+        int intentos = 0;
 
         Console.Write("Cadena: ");
         adivinar = Console.ReadLine();
+        //Como en un string no se puede sustituir letra a letra he creado un array
+        //que cada valor será cada letra de la palabra
         char[] ahorcado = new char[adivinar.Length];
-        int intentos = 0;
-        bool resuelto = false;
-
+        
+        //Lleno el array con tantos guiones como longitud de palabra vayamos a adivinar
         for (int i = 0; i < adivinar.Length; i++)
         {
             ahorcado[i] = '_';
         }
-
+        
+        //Va a seguir pidiendo letras y mostrando el ahorcado hasta que falle 6 veces (o acierte)
         do
         {
             Console.WriteLine();
@@ -31,14 +34,34 @@ public class Practica2
 
             if (adivinar.Contains(letra))
             {
+                //1º for: va sustituyendo "_" por la letra que meta el usuario si coincide
                 for (int i = 0; i < adivinar.Length; i++)
                 {
                     if (adivinar.IndexOf(letra, i) >= 0)
                     {
                         ahorcado[adivinar.IndexOf(letra, i)] = letra;
                     }
+
+                }
+                //La variable palabra me va a servir para comparar el resultado final del array
+                string palabra = "";
+
+                //2º for: El array contiene "elementos sueltos", por lo que vamos a
+                //"convertirlo" en una palabra (variable string) y así poder compararla luego
+                for (int i = 0; i < adivinar.Length; i++)
+                {
+                    palabra = palabra + Convert.ToString(ahorcado[i]);
+                }
+
+                //Si la palabra ya coincide con la que ibamos a adivinar sale del bucle
+                if (palabra == adivinar)
+                {
+                    Console.WriteLine("¡¡¡ENHORABUENA!!!");
+                    break;
                 }
             }
+
+            //Si falla le mostrará el dibujo correspondiente en funcion de los fallos cometidos
             else
             {
                 intentos++;
@@ -109,15 +132,9 @@ public class Practica2
 
         } while (intentos < 6);
 
-        if (intentos == 6)
-        {
-            Console.WriteLine();
-            Console.WriteLine("R.I.P. La palabra era {0}", adivinar);
-        }
-
-        else
-        {
-            Console.WriteLine("¡¡¡ENHORABUENA!!!");
-        }
+        //Si comete mas de 6 fallos termina el juego
+        Console.WriteLine();
+        Console.WriteLine("R.I.P. La palabra era {0}", adivinar);
+        
     }
 }
